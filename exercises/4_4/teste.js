fe3cafe8237ee8b40827ee7205df5aacb9fa533c
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-07-21 21:18:23 -0300

    Adição de arquivos da aula 4.4 e 5.1

---
+++ exercises/4_4/teste.js
@@ -0,0 +1,17 @@
+git 
+    
+
+// function verificaPalindrome(palavra) {
+//     let arrayLetras = palavra.split("");
+//     let isPalindrome = true;
+//     for (let index in arrayLetras) {
+//       if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+//         isPalindrome = false;
+//       }
+//     }
+//     return isPalindrome;
+//   }
+// let pos = palavra.length;
+// for(let i=0;i<=palavra.length; i +=1){
+// console.log (palavra[pos])
+// pos -= 1}
