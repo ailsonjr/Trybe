d1be9e9dd13a098b573170306254a62a8811a574
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-11 18:10:19 -0300

    Adicionado pasta 7_2 na pasta conteudo e os arquivos js parte 1 e 2 abordados no conteúdo

---
+++ conteudo/7_2/parte2.js
@@ -0,0 +1,26 @@
+// Object.keys
+
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsWithValues(student));
+  
