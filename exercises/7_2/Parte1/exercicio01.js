876485f103c509f64e974809ba4d6564335a3e16
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-11 18:42:07 -0300

    Adicionado pasta 7_2 a pasta exercises contendo os exercicios 1 e 2 da parte 1

---
+++ exercises/7_2/Parte1/exercicio01.js
@@ -0,0 +1,14 @@
+//Use the playerNumber variable to look up player 16 in testObj using bracket notation. Then assign that name to the player variable.
+
+// Setup
+let testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  let playerNumber = 16;       // Change this line
+  let player = testObj[playerNumber];   // Change this line
+  
