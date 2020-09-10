39b4443675fd44fc2bfb50fd3edc8c5465d4bb1d
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-10 19:20:54 -0300

    Criado 2 arquivos contendo exemplos dados no site: https://pt-br.reactjs.org/docs/components-and-props.html\#composing-components

---
+++ conteudo/11_2/comment.js
@@ -0,0 +1,81 @@
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+/* Component comment completo
+ - Começo do component
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+- Fim do component*/
+
+/*  Esse componente pode ser complicado de alterar por causa de todo o aninhamento. Ele aceita author (um objeto), text (uma string) e date (uma data) como props e descreve um comentário em um site de mídia social.Também é difícil reutilizar suas partes individuais. Vamos extrair alguns componentes dele */
+
+// Componet Comment divido em partes
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+
+
+// Parte para renderizar o component
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
