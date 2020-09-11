23f4f020bdad61960a23027912cc17705e7376b7
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-11 17:52:51 -0300

    Implementado código do arquivo Pokemon.js para que o mesmo renderize a lista com os dados dos pokemons

--- exercises/11_2/my-pokedex/src/Pokedex.js
+++ exercises/11_2/my-pokedex/src/Pokedex.js
@@ -1,3 +1,12 @@
 import React, { Component } from 'react';
+import Pokemon from './Pokemon';
+import pokemons from './data';
 
-class Pokedex extends Component {}
+class Pokedex extends Component {
+  render () {
+    return 
+      
+  }
+}
+
+export default Pokedex;
--- exercises/11_2/my-pokedex/src/Pokemon.js
+++ exercises/11_2/my-pokedex/src/Pokemon.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Pokemon extends Component {
   render() {
-    
+    const { name, type, averageWeight, image } = this.props;
+    return (
+      <div>
+        <ul>
+          <li>{name}</li>
+          <li>{type}</li>
+          <li>{averageWeight.value}</li>
+          <li>{averageWeight.measurementUnit}</li>
+        </ul>
+      </div>
+    )
   }
 }
+export default Pokemon;
