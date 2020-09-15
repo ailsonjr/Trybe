67604aaad0ef5762c7a16a468208731ef78c041b
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-15 18:03:44 -0300

    Editado código dos arquivos Pokemon.js, data.js, App.js e index.html para renderizar a lista de pokemons da pokedex e estilizar alguns conteúdos

--- exercises/11_2/my-pokedex/src/App.js
+++ exercises/11_2/my-pokedex/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
+    <div className='pokemon-div'>
       <Pokedex />
     </div>
   );
--- exercises/11_2/my-pokedex/src/Pokemon.js
+++ exercises/11_2/my-pokedex/src/Pokemon.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import './Pokemon.css';
 import PropTypes from 'prop-types';
 
 class Pokemon extends Component {
   render() {
     const { name, type, averageWeight, image } = this.props;
     return (
-      <div>
+      <div className='pokemon-div'>
         <h3>{name}</h3>
         <h4>{type}</h4>
         <h4>{averageWeight.value}{averageWeight.measurementUnit}</h4>
-        <img src={image}></img>
+        <img src={image} alt='pokemon-img'></img>
       </div>
     )
   }
+}
+
+Pokemon.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  averageWeight: PropTypes.number
 }
 export default Pokemon;
