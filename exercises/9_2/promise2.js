2c7568c83ff850389d04915f3965743e8d08c020
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-12-13 17:57:49 -0300

    Criado um novo arquivo, promise2.js, para conter o código do último exercício feito e o arquivo promise.js voltou ao formato que estava antes da última implementação para assim poder os exercícios em arquivos distintos

--- exercises/9_2/promise2.js
+++ exercises/9_2/promise2.js
@@ -1,6 +1,6 @@
 const promise = new Promise((resolve, reject) => {
   const array = [];
-  
+
   for (let i = 0; i < 10; i += 1) {
     const number = Math.floor(Math.random() * (50 - 1) + 1);
     array.push(number);
@@ -8,11 +8,16 @@ const promise = new Promise((resolve, reject) => {
 
   const sum = array.map(num => num ** 2)
     .reduce((acc, elem) => acc + elem);
-  
+
   if (sum < 8000) {
     return resolve(sum);
   }
   reject(sum)
 })
-  .then(number => console.log(`A soma dos elementos da array é ${number} e é menor que 8000`))
+  .then(number => {
+    const divisors = [2, 3, 5, 10];
+
+    const newArray = divisors.map(numbers => number / numbers);
+    return console.log(`A soma dos elementos da array é ${number} e resultado da divisão desse número por 2, 3, 5 e 10 em um array é [ ${newArray} ]`);
+  })
   .catch(number => console.log(`Erro: A soma dos elementos da array é ${number} e é maior que 8000`));
