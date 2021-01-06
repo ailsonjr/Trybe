e12bed1c7bd81b0c1415150400a4196226e383cf
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-05 21:31:12 -0300

    Adicionado ao arquivo usersFunctions.test.js lógica para teste usando async/await, atendendo assim os requisitos do exercício 3

--- exercises/10_2_TestesAssincronos/usersFunctions.test.js
+++ exercises/10_2_TestesAssincronos/usersFunctions.test.js
@@ -1,4 +1,3 @@
-const { expect } = require('@jest/globals');
 const getUserName = require('./usersFunctions')
 
 describe('When the userID is entered', () => {
@@ -13,5 +12,22 @@ describe('When the userID is entered', () => {
     return getUserName(3).catch(error => {
       expect(error).toEqual({ error: 'User with ' + 3 + ' not found.' });
     });
+  });
+});
+
+describe('When the userID is entered using async/await', () => {
+  it('userID is found and shows the user name', async () => {
+    expect.assertions(1);
+    await getUserName(4).then(user => {
+      expect(user).toBe('Mark');
+    });
+  });
+  it('userId is not found, show a error message', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(3);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with ' + 3 + ' not found.' });
+    }
   });
 });
