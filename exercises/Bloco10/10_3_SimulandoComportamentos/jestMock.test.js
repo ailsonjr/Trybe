55c3c5099537bc7fe6f50b68542fc04b32293d1d
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-06 20:13:49 -0300

    Criado nova implementação para que a função receba 3 parametros retorne sua multiplicação

--- exercises/Bloco10/10_3_SimulandoComportamentos/jestMock.test.js
+++ exercises/Bloco10/10_3_SimulandoComportamentos/jestMock.test.js
@@ -9,13 +9,28 @@ test('Faz teste mocado onde troca a funcionalidade da função e a testa', () =>
   randomNumber(10, 5);
 
   expect(randomNumber).toHaveBeenCalled();
-   expect(randomNumber).toHaveBeenCalledTimes(1);
-   expect(randomNumber).toHaveBeenCalledWith(10, 5);
-   expect(randomNumber(10, 5)).toBe(2);
-    })
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber).toHaveBeenCalledWith(10, 5);
+  expect(randomNumber(10, 5)).toBe(2);
+})
 
 // 3 - Crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
 
+test('Função randomNumber retorna a multipicação de 3 números', () => {
+  randomNumber.mockImplementation((num1, num2, num3) => num1 * num2 * num3);
+  randomNumber(2, 4, 5);
+  randomNumber(1, 1, 1);
+
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledTimes(4);
+  expect(randomNumber).toHaveBeenCalledWith(2, 4, 5);
+  expect(randomNumber(2, 4, 5)).toBe(40)
+
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledTimes(5);
+  expect(randomNumber).toHaveBeenCalledWith(1, 1, 1);
+  expect(randomNumber(1, 1, 1)).toBe(1)
+});
 
 // 4 - Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.
 // 5 - Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
