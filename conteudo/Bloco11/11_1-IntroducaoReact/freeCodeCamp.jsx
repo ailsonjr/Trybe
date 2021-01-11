119fd6deec140ce1594512d4c22a133956fbd41b
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-11 18:07:22 -0300

    Editado a formatação do arquivo freeCodeCamp.jsx na pasta dos arquivos dos conteudos

--- conteudo/Bloco11/11_1-IntroducaoReact/freeCodeCamp.jsx
+++ conteudo/Bloco11/11_1-IntroducaoReact/freeCodeCamp.jsx
@@ -5,13 +5,15 @@ const JSX = <h1>Hello JSX!</h1>;
 // 2 - Define a new constant JSX that renders a div which contains the following elements in order:
 // An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
 
-const JSX = 
-<div>
-    <h1>Elements List</h1>
-    <p>List</p>
-    <ul>
-        <li>Element 1;</li>
-        <li>Element 2;</li>
-        <li>Element 3</li>
-    </ul>
-</div>
+const JSX = (
+    <div>
+        <h1>Elements List</h1>
+        <p>List</p>
+        <ul>
+            <li>Element 1;</li>
+            <li>Element 2;</li>
+            <li>Element 3</li>
+        </ul>
+    </div>
+);
+
