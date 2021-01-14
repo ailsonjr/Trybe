4dab10c73419a41313d32d25fd7969b882ab206d
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-14 17:18:15 -0300

    Criado arquivo 12_1-estados-eventos em React para salvar os arquivos usados como exemplo e para fixação do conteúdo

---
+++ conteudo/Bloco12/12_1-estados-eventos/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FancyButtons from './FancyButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <FancyButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ conteudo/Bloco12/12_1-estados-eventos/src/FancyButtons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+function handleClick() {
+  console.log('Você clicou');
+}
+
+function handleClick2() {
+  console.log('React é muito divertido!');
+}
+
+function handleClick3() {
+  console.log('React fica cada vez mais fácil');
+}
+
+class FancyButtons extends React.Component {
+  /* Repare que, diferentemente do HTML, no
+  JSX você associa uma função a um evento
+  passando a própria função entre chaves `{}` */
+  render() {
+    return (
+      <div>
+        <h3>Componente FancyButtons</h3>
+        <p>Evento com 3 botões onde cada um imprime no console uma mensagem diferentegit</p>
+        <button onClick={handleClick}>Meu botão1</button>
+        <button onClick={handleClick2}>Meu botão2</button>
+        <button onClick={handleClick3}>Meu botão3</button>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default FancyButtons;
