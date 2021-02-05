023b6d960b31d530e8b26fc9d0dc1d619926d6cb
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-05 14:49:04 -0300

    Atualizado pasta com os arquivos usados nos exemplos do conteúdo

---
+++ conteudo/Bloco15/src/App.js
@@ -0,0 +1,12 @@
+import Conteudo1 from './RTL/Conteudo1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Conteudo1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ conteudo/Bloco15/src/RTL/Conteudo1.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ValidEmail from './ValidEmail';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+        </label>
+        <input
+          id="id-email"
+          value={email}
+          type="email"
+          onChange={(e) => this.changeEmail(e.target.value)}
+        />
+        <input
+          id="btn-send"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={() => this.changeSaveEmail(email)}
+        />
+        <input id="btn-back" type="button" value="Voltar" />
+        <ValidEmail email={saveEmail} />
+      </div>
+    );
+  }
+}
