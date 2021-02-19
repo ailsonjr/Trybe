7db9f41fb9bd3052df80c017ff5e314f419cb7e1
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-19 15:12:34 -0300

    Criado ambiente para fazer a primeira parte dos exercícios

---
+++ exercises/Bloco15/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Exercise from "./15_1-RTL/exercise-one/src/Exercise";
+
+function App() {
+  return (
+    <div className="App">
+      <Exercise />
+    </div>
+  );
+}
+
+export default App;
