0f6cc5986994adb31763b38585851c38f564819f
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-22 16:04:02 -0300

    Criado branch com o código inicial da aula ao vivo

---
+++ conteudo/Bloco16/src/16_3-ReactReduxPratica/liveLecture/src/products.js
@@ -0,0 +1,7 @@
+export default [
+  { name: 'Senhor dos Anéis', price: 17.99, stockQuantity: 12 },
+  { name: 'Harry Potter', price: 4.99, stockQuantity: 30 },
+  { name: 'Crepúsculo', price: 49.99, stockQuantity: 3 },
+  { name: 'As Crônicas do Rei Artur', price: 19.99, stockQuantity: 1 },
+  { name: 'O guia do mochileiro das Galáxias', price: 42.00, stockQuantity: 42 },
+];
